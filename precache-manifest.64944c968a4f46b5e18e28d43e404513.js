f0f87abfd87b63adb2bcbcaddefeedae77f0fc13
gabespound <gabespound@gmail.com>
2018-11-02 19:42:18 -0700

    Updates

---
+++ precache-manifest.64944c968a4f46b5e18e28d43e404513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "106e0a3004028eb09c14",
+    "url": "/contactmanager/static/js/main.106e0a30.chunk.js"
+  },
+  {
+    "revision": "2ac046c7ca9d6823b6cc",
+    "url": "/contactmanager/static/js/1.2ac046c7.chunk.js"
+  },
+  {
+    "revision": "106e0a3004028eb09c14",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2ac046c7ca9d6823b6cc",
+    "url": "/contactmanager/static/css/1.7982309c.chunk.css"
+  },
+  {
+    "revision": "a5199484241bac15140b8639d0fc47c0",
+    "url": "/contactmanager/index.html"
+  }
+];
